a2bde52a31f095d0c5d99ae92ef6cea9bf816b92
Saksham Khatiwada <155241009+ItsSaksham00@users.noreply.github.com>
2024-01-12 12:42:12 +0545

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import './globals.css'
 const Pop = Poppins({ subsets: ['latin'], weight: ["400","500"] })
 
 export const metadata = {
-  title: 'Portfolio - Manoj Gautam',
-  description: 'Hello from Manjeyy',
+  title: 'Portfolio - Saksham Khatiwada',
+  description: 'Hello from Saksham',
 }
 
 export default function RootLayout({ children }) {
